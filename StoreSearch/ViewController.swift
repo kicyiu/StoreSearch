3d15d75f56ea1585fc83d1a5b5ecb94ae652ab70
kicyiu <albertokicyiu@gmail.com>
2018-12-09 21:05:48 -0300

    Initial Commit

---
+++ StoreSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Alberto Tsang on 12/9/18.
+//  Copyright © 2018 kicyiusoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
