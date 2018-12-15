4569fd0f633ed2feb55498e3383a2d36c3295b34
kicyiu <albertokicyiu@gmail.com>
2018-12-15 13:45:13 -0300

    Add a search bar and table view

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Alberto Tsang on 12/15/18.
+//  Copyright © 2018 kicyiusoft. All rights reserved.
+//
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
