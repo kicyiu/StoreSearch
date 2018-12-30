7055256f6ac219fe8403eb8f80a08b71cf6bb8d3
kicyiu <albertokicyiu@gmail.com>
2018-12-30 17:40:05 -0300

    Animation for present and dismiss popup

--- StoreSearch/DimmingPresentationController.swift
+++ StoreSearch/DimmingPresentationController.swift
@@ -12,6 +12,29 @@ import UIKit
 
 //You’re providing your own version that overrides some of this behavior, in particular telling UIKit to leave the SearchViewController visible
 class DimmingPresentationController: UIPresentationController {
+    
+    lazy var dimmingView = GradientView(frame: CGRect.zero)
+    
+    override func presentationTransitionWillBegin() {
+        dimmingView.frame = containerView!.bounds
+        containerView!.insertSubview(dimmingView, at: 0)
+        
+        dimmingView.alpha = 0
+        if let coordinator = presentedViewController.transitionCoordinator {
+            coordinator.animate(alongsideTransition: { _ in
+                self.dimmingView.alpha = 1
+            }, completion: nil)
+        }
+    }
+    
+    override func dismissalTransitionWillBegin() {
+        if let coordinator = presentedViewController.transitionCoordinator {
+            coordinator.animate(alongsideTransition: { _ in
+                self.dimmingView.alpha = 0
+            }, completion: nil)
+        }
+    }
+    
     override var shouldRemovePresentersView: Bool {
         return false
     }
---
+++ StoreSearch/SlideOutAnimationController.swift
@@ -0,0 +1,33 @@
+//
+//  SlideOutAnimationController.swift
+//  StoreSearch
+//
+//  Created by Alberto Tsang on 12/30/18.
+//  Copyright © 2018 kicyiusoft. All rights reserved.
+//
+
+import UIKit
+
+class SlideOutAnimationController: NSObject,
+UIViewControllerAnimatedTransitioning {
+    
+    func transitionDuration(using transitionContext:
+        UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.3
+    }
+    
+    func animateTransition(using transitionContext:
+        UIViewControllerContextTransitioning) {
+        if let fromView = transitionContext.view(forKey:
+            UITransitionContextViewKey.from) {
+            let containerView = transitionContext.containerView
+            let duration = transitionDuration(using: transitionContext)
+            UIView.animate(withDuration: duration, animations: {
+                fromView.center.y -= containerView.bounds.size.height
+                fromView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+            }, completion: { finished in
+                transitionContext.completeTransition(finished)
+            })
+        }
+    }
+}
